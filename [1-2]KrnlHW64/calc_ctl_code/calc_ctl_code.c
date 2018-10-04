3c1f1749b2957d597b25b7e303ece0fb370146fd
satanklaus <empated@mgail.com>
2018-10-04 15:20:45 +0300

    vedone

--- [1-2]KrnlHW64/calc_ctl_code/calc_ctl_code.c
+++ [1-2]KrnlHW64/calc_ctl_code/calc_ctl_code.c
@@ -6,13 +6,13 @@ typedef int32_t 		DWORD;
 #define METHOD_BUFFERED 	0x0
 #define FILE_ANY_ACCESS 	0x0
 
-#define CTL_CODE_GEN(LNG) (FILE_DEVICE_UNKNOWN << 16 | FILE_ANY_ACCESS << 14 | LNG << 2 | METHOD_BUFFERED)
+#define CTL_CODE_GEN(FCODE) (FILE_DEVICE_UNKNOWN << 16 | FILE_ANY_ACCESS << 14 | FCODE << 2 | METHOD_BUFFERED)
 
 int main(DWORD argc, char *argv[])
 {
-	DWORD lng = 0x0;
-	if (argc ==2 && sscanf(argv[1],"%X", &lng)) 
-		printf("Output: %ld(0x%X)\n",CTL_CODE_GEN(lng),CTL_CODE_GEN(lng));
+	DWORD function_code = 0x0;
+	if (argc ==2 && sscanf(argv[1],"%X", &function_code)) 
+		printf("Output: %ld(0x%X)\n",CTL_CODE_GEN(function_code),CTL_CODE_GEN(function_code));
 	else 
-		printf("Use 0xAABBCCDD as argument.\n");
+		printf("pass hex number as argument\n");
 }
